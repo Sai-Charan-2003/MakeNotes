43042694a24f3fd461d2e2ac71fc4316712c4ea0
Sai-Charan-2003 <charanmetla13@gmail.com>
2023-07-15 16:48:43 +0530

    env variables

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const dbConnection = async () => {
 dbConnection();
 
 app.use((req, res, next) => {
-  res.setHeader(`Access-Control-Allow-Origin", ${process.env.FRONTEND_URL}`);
+  res.setHeader("Access-Control-Allow-Origin", `${process.env.FRONTEND_URL}`);
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
